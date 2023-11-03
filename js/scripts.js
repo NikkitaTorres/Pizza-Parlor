575bd4922625e39eb9fa1a402919848aa8a38de7
NikkitaTorres <nikkitatorres@yahoo.com>
2023-11-03 09:59:55 -0700

    Add code to pass the second test

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,19 @@ function Pizza(pizzaSize, pizzaToppings) {
   this.pizzaToppings = pizzaToppings;
 }
 
+function calculateTotalCost(size, toppings) {
+  const sizePrices = {
+    "Small ($3.00)": 3.00,
+    "Medium ($5.00)": 5.00,
+    "Large ($7.00)": 7.00,
+    "Xtra-Large ($10.00)": 10.00,
+  };
+
+  const sizeCost = sizePrices[size];
+
+  const toppingsCost = toppings.length;
+
+  const totalCost = sizeCost + toppingsCost;
+
+  return totalCost;
+}
